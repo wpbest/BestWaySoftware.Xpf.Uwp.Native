824c0f9804a338ebda3795f06179cd64005b6d14
William Best <william.best@jeppesen.com>
2016-03-26 22:04:38 -0600

    Initial commit

---
+++ CleanBuildAll.cmd
@@ -0,0 +1,12 @@
+msbuild BestWaySoftware.Xpf.Uwp.Native.sln /t:Clean /p:Platform=ARM /p:Configuration=Debug
+msbuild BestWaySoftware.Xpf.Uwp.Native.sln /t:Clean /p:Platform=ARM /p:Configuration=Release
+msbuild BestWaySoftware.Xpf.Uwp.Native.sln /t:Clean /p:Platform=x86 /p:Configuration=Debug
+msbuild BestWaySoftware.Xpf.Uwp.Native.sln /t:Clean /p:Platform=x86 /p:Configuration=Release
+msbuild BestWaySoftware.Xpf.Uwp.Native.sln /t:Clean /p:Platform=x64 /p:Configuration=Debug
+msbuild BestWaySoftware.Xpf.Uwp.Native.sln /t:Clean /p:Platform=x64 /p:Configuration=Release
+msbuild BestWaySoftware.Xpf.Uwp.Native.sln /t:Build /p:Platform=ARM /p:Configuration=Debug
+msbuild BestWaySoftware.Xpf.Uwp.Native.sln /t:Build /p:Platform=ARM /p:Configuration=Release
+msbuild BestWaySoftware.Xpf.Uwp.Native.sln /t:Build /p:Platform=x86 /p:Configuration=Debug
+msbuild BestWaySoftware.Xpf.Uwp.Native.sln /t:Build /p:Platform=x86 /p:Configuration=Release
+msbuild BestWaySoftware.Xpf.Uwp.Native.sln /t:Build /p:Platform=x64 /p:Configuration=Debug
+msbuild BestWaySoftware.Xpf.Uwp.Native.sln /t:Build /p:Platform=x64 /p:Configuration=Release
