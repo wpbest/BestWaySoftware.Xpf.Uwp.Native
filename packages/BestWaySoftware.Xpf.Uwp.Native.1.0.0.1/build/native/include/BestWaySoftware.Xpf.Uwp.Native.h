6d701d35ac5715aa89e85b606fca8f61a77a5f42
wpbest <wpbest@gmail.com>
2016-04-08 15:41:16 -0600

    Initial commit

---
+++ packages/BestWaySoftware.Xpf.Uwp.Native.1.0.0.1/build/native/include/BestWaySoftware.Xpf.Uwp.Native.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#ifdef XPF_EXPORTS
+#define XPF_API __declspec(dllexport)
+#else
+#define XPF_API __declspec(dllimport)
+#endif
+
+class XPF_API xpf
+{
+public:
+	int getVersion();
+
+};
